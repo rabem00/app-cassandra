e0437770023ee2ad5c69796d5e65c89f364616a4
M Rabelink <marco.rabelink@icloud.com>
2017-02-12 18:47:43 +0100

    Minor cassandra connection add on

--- Cassandra/main.go
+++ Cassandra/main.go
@@ -13,7 +13,10 @@ func init() {
 	var err error
 
 	cluster := gocql.NewCluster("192.168.64.6")
+	cluster.ProtoVersion = 4
+	cluster.CQLVersion = "3.0.0"
 	cluster.Keyspace = "appdemo"
+	cluster.Consistency = gocql.Quorum
 	Session, err = cluster.CreateSession()
 	if err != nil {
 		panic(err)
